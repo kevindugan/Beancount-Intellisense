981feaa92c1269c54b7e4f21395e7602bee5ac92
Kevin J. Dugan <kevin.dugan@woolpert.com>
2023-09-29 08:59:41 -0700

    Adding clear message for test pass/fail

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -4,3 +4,6 @@ export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 
 node "$(pwd)/client/out/test/runTest"
+
+# Echo clear message on whether tests passed
+if [ $? == 0 ]; then echo "\033[0;32mPassed\033[0m"; else echo "\033[0;31mFailed\033[0m"; fi
